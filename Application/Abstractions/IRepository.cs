293bde5e00cbc27d107abf14feb41bf6811ae2e9
LordWile <lordwilee@outlook.com>
2025-11-24 00:25:06 +0300

    Updated ef repository

--- Application/Abstractions/IRepository.cs
+++ Application/Abstractions/IRepository.cs
@@ -1,10 +1,12 @@
-﻿namespace Application.Abstractions;
+﻿using System.Linq.Expressions;
+
+namespace Application.Abstractions;
 
 public interface IRepository<T> where T : class
 {
-    IQueryable<T> Query(bool tracked = false);
-    Task<T?> GetByIdAsync(Guid id, CancellationToken ct);
-    Task AddAsync(T entity, CancellationToken ct);
-    void Update(T entity);
-    void Remove(T entity);
+	IQueryable<T> Query(bool tracked = false);
+	Task<T?> GetByIdAsync(Guid id, CancellationToken ct);
+	Task AddAsync(T entity, CancellationToken ct);
+	void Update(T entity);
+	void SoftDelete(T entity);
 }
