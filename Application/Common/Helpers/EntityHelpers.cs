70477101541def9b7a8eaddfe345b72bb0e1b155
LordWile <lordwilee@outlook.com>
2025-12-01 00:40:18 +0300

    created indexBody Updated service codes and added new excetions

---
+++ Application/Common/Helpers/EntityHelpers.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Helpers
+{
+	public static class EntityHelpers
+	{
+
+		public static T EnsureFound<T>(this T? entity, string message)
+		where T : class
+		{
+			if(entity is null)
+				throw new NotFoundAppException(message);
+
+			return entity;
+		}
+	}
+}
