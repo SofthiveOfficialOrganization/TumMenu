b926e35ddda8264b8af86f55c776b67f9a20c467
LordWile <lordwilee@outlook.com>
2025-12-01 00:40:04 +0300

    Added missing menu configuration and commands.

---
+++ Application/Menus/Commands/UpdateMenuCommand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using MapsterMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Menus.Commands;
+
+public sealed record UpdateMenuCommand(
+	Guid Id,
+	string? Name
+) : IRequest<Menu>, ITransactionalRequest;
+
+public class UpdateMenuCommandHandler(
+	IRepository<Menu> repoMenu,
+	IMapper mapper
+) : IRequestHandler<UpdateMenuCommand, Menu>
+{
+	public async Task<Menu> Handle(UpdateMenuCommand req, CancellationToken ct)
+	{
+		var menu = await repoMenu.GetByIdAsync(req.Id, ct);
+		if(menu is null)
+			throw new KeyNotFoundException("Menü bulunamadı.");
+		if(req.Name is not null)
+			menu.Name = req.Name;
+
+		repoMenu.Update(menu);
+		return menu;
+	}
+}
---
+++ Application/Menus/MenuMappingProfiles.cs
@@ -0,0 +1,23 @@
+﻿using Application.Menus.Commands;
+using Application.Menus.DTOs;
+using Application.Owners.DTOs;
+using Domain.Entities;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Menus;
+
+public class MenuMappingProfiles
+{
+	public void Register(TypeAdapterConfig config)
+	{
+		config.NewConfig<Menu, MenuDTO>();
+		config.NewConfig<CreateMenuToCompanyCommand, Menu>();
+		config.NewConfig<CreateMenuToStoreCommand, Menu>();
+		config.NewConfig<CopyMenuCommand, Menu>();
+	}
+}
