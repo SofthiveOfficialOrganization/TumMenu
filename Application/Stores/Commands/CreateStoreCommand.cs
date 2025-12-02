9400c9b64ca57a5034af8975196b503dca61a435
LordWile <lordwilee@outlook.com>
2025-12-02 22:47:28 +0300

    Added new existance check method.

--- Application/Stores/Commands/CreateStoreCommand.cs
+++ Application/Stores/Commands/CreateStoreCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Abstractions;
+using Application.Common.Exceptions;
 using Application.Stores.DTOs;
 using Domain.Entities;
 using MapsterMapper;
@@ -8,22 +9,26 @@ namespace Application.Stores.Commands;
 
 public sealed record CreateStoreCommand
 (
-    string Name,
-    string Slug,
-    string PhoneNumber,
-    Guid CompanyId
+	string Name,
+	string Slug,
+	string PhoneNumber,
+	Guid CompanyId
 ) : IRequest<StoreDTO>, ITransactionalRequest;
 
 public class CreateStoreCommandHandler(
-    IRepository<Store> repoStore,
-    IMapper mapper
+	IRepository<Store> repoStore,
+	IMapper mapper,
+	IRepository<Company> repoCompany
 ) : IRequestHandler<CreateStoreCommand, StoreDTO>
 {
-    public async Task<StoreDTO> Handle(CreateStoreCommand req, CancellationToken ct)
-    {
-        var store = mapper.Map<Store>(req);
-        await repoStore.AddAsync(store, ct);
-        var storeDTO = mapper.Map<StoreDTO>(store);
-        return storeDTO;
-    }
+	public async Task<StoreDTO> Handle(CreateStoreCommand req, CancellationToken ct)
+	{
+		bool companyExists = await repoCompany.ExistsAsync(c => c.Id == req.CompanyId, ct);
+		if(!companyExists)
+			throw new UnprocessableAppException("Dükkanın ekleneceği şirket bulunamadı.");
+		var store = mapper.Map<Store>(req);
+		await repoStore.AddAsync(store, ct);
+		var storeDTO = mapper.Map<StoreDTO>(store);
+		return storeDTO;
+	}
 }
