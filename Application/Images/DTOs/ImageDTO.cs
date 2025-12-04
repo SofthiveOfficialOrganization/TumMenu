a6d53b2bb6c341d79dcbc82f816cbd16b5d971fe
LordWile <lordwilee@outlook.com>
2025-12-05 00:14:57 +0300

    Added Image services

---
+++ Application/Images/DTOs/ImageDTO.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Base.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Images.DTOs;
+
+public sealed class ImageDTO : BaseDTO
+{
+	public string ImageLink { get; set; } = null!;
+	public string? AltText { get; set; }
+	public int SortOrder { get; set; }
+	public Guid ReferenceId { get; set; }
+	public ImageRefType Type { get; set; } = ImageRefType.Unknown;
+	public string Slot { get; set; } = "default-gallery";
+	public int? Width { get; set; }
+	public int? Height { get; set; }
+}
---
+++ Application/Images/Queries/GetImagesByReferanceIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Abstractions;
+using Application.Common.Base.DTOs;
+using Application.Common.Base.Page.RequestBase;
+using Application.Images.DTOs;
+using Domain.Entities;
+using MapsterMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Images.Queries;
+
+public sealed record GetImagesByReferanceIdQuery(
+	Guid ReferanceId,
+	ImageRefType? Type
+) : PageRequest, IRequest<PaginatedListDTO<ImageDTO>>;
+
+public class GetImagesByReferanceIdHandler(
+	IRepository<Image> repoImage,
+	IMapper mapper
+) : IRequestHandler<GetImagesByReferanceIdQuery, PaginatedListDTO<ImageDTO>>
+{
+	public async Task<PaginatedListDTO<ImageDTO>> Handle(GetImagesByReferanceIdQuery req, CancellationToken ct)
+	{
+		var images = await repoImage.GetPageListAsync(
+			req,
+			image => image.ReferenceId == req.ReferanceId && (req.Type != null && image.Type == req.Type),
+			orderBy: image => image.OrderBy(image => image.SortOrder),
+			ct: ct
+		);
+		var imagesDTO = mapper.Map<PaginatedListDTO<ImageDTO>>(images);
+		return imagesDTO;
+	}
+}
